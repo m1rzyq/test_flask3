82731c7a04139031769628a7bb6d9328679ea27c
syaifulnizarrr <59416896+syaifulnizarrr@users.noreply.github.com>
2021-12-02 04:22:34 +0800

    aaaaaaa fixed

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.9.7-alpine3.13
-COPY . /
-WORKDIR /
+COPY . /python_flask
+WORKDIR /python_flask
 RUN pip install -r requirements.txt
--- templates/main.py
+++ templates/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template, request
 
-app = Flask(__name__, render_template='template')
+app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
